1b32966b596c4a15c07549059437ae1e1b211d61
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2023-07-02 10:04:32 +0530

    Update bfub.py
    removing totally

--- bfub.py
+++ bfub.py
@@ -4,7 +4,6 @@ import wget
 import requests
 import os
 import time
-import speedtest
 import re
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -52,63 +51,6 @@ async def url(_, message):
         print(str(e))
         await m.edit(str(e))
         return
-
-def convert(speed):
-    return round(int(speed) / 1048576, 2)
-
-@app.on_message(filters.command("speedtest"))
-async def speedtestxyz(client, message):
-    buttons = [[InlineKeyboardButton("Image",
-                                    callback_data="speedtest_image"),
-                InlineKeyboardButton("Text",
-                                    callback_data="speedtest_text")]]
-    await message.reply_text(
-        "Select SpeedTest Mode",
-        reply_markup=InlineKeyboardMarkup(buttons))
-
-
-def speed_convert(size):
-    """Hi human, you can't read bytes?"""
-    power = 2 ** 10
-    zero = 0
-    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
-    while size > power:
-        size /= power
-        zero += 1
-    return f"{round(size, 2)} {units[zero]}"
-
-
-def speedtest_callback(_, __, query):
-    if re.match("speedtest", query.data):
-        return True
-
-
-speedtest_create = filters.create(speedtest_callback)
-
-
-@app.on_callback_query(speedtest_create)
-async def speedtestxyz_callback(client, query):
-        await query.message.edit_text('Runing a speedtest....')
-        speed = speedtest.Speedtest()
-        speed.get_best_server()
-        speed.download()
-        speed.upload()
-        replymsg = 'SpeedTest Results:'
-
-        if query.data == 'speedtest_image':
-            speedtest_image = speed.results.share()
-            replym = f"[SpeedTest Results:]({speedtest_image})"
-            await query.message.edit_text(replym, parse_mode="markdown")
-
-        elif query.data == 'speedtest_text':
-            result = speed.results.dict()
-            replymsg += f"\n - **ISP: {result['client']['isp']}"
-            replymsg += f"\n - Download: {speed_convert(result['download'])}"
-            replymsg += f"\n - Upload: {speed_convert(result['upload'])}"
-            replymsg += f"\n - Ping: {result['ping']}"
-            await query.message.edit_text(replymsg, parse_mode="markdown")    
-   
-
 @app.on_message(filters.command("bay") & filters.chat(sudo_chats_id))
 async def tg(_, message):
     now = time.time()
